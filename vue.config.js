a3976160d93acd37c11d4fd64de376bdd329c940
陈世亮 <352056038@qq.com>
2020-12-09 10:07:08 +0800

    bug修复

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,14 @@
 /*
  * @name:
  * @Date: 2020-11-30 16:59:39
- * @LastEditTime: 2020-11-30 17:08:44
- * @FilePath: \fy_erp_vue3\vue.config.js
+ * @LastEditTime: 2020-12-09 10:06:36
+ * @FilePath: \vue3-typescript-element-admin\vue.config.js
  * @permission:
  */
-
-module.exports = function() {
-  return {
-    devServer: {
-      port: 8088,
-      open: true
-    }
-  };
+module.exports = {
+  productionSourceMap: false,
+  devServer: {
+    port: 8088,
+    open: true
+  }
 };
